48cb825fd509d4fbb9c70bd6d6336e75d23b5ad7
ardi triana <39799235+arditriana01@users.noreply.github.com>
2021-06-15 23:47:31 +0800

    Update data.py

--- data.py
+++ data.py
@@ -1,15 +1,37 @@
 import tkinter as tk
 
-def main(root):
-    btnLihat = tk.Button(root, text="Lihat Data", height=5, width=35)
-    btnLihat.grid(row=2, column=2)
+kolom = 'Nama', 'NIM', 'Prodi', 'Nilai'
 
-    btnTambah = tk.Button(root, text="Tambah Data", height=5, width=40)
-    btnTambah.grid(row=2, column=3)
+def ambil_data(inputData):
+    for input in inputData:
+        data = input[0]
+        text  = input[1].get()
+        label3 = tk.Label(root, text= data)
+        label3.pack()
+        
+        label2 = tk.Label(root, text=text)        
+        label2.pack()
 
-root = tk.Tk()
-root.title(('Data Mahasiswa'))
-root.geometry('500x350')
+def input_data(root, kolom):
+    inputData = []
+    for data in kolom:
+        baris = tk.Frame(root)
 
-main(root)
-root.mainloop()
+        lblText = tk.Label(baris, width=15, text=data, anchor='w')
+
+        input = tk.Entry(baris)
+        baris.pack(side=tk.TOP, fill=tk.X, padx=5, pady=5)
+
+        lblText.pack(side=tk.LEFT)
+        input.pack(side=tk.RIGHT, expand=tk.YES, fill=tk.X)
+        inputData.append((data, input))
+    return inputData
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    ents = input_data(root, kolom)
+    root.bind('<Return>', (lambda event, e=ents: ambil_data(e)))   
+    b1 = tk.Button(root, text='Tambah Data',
+                  command=(lambda e=ents: ambil_data(e)))
+    b1.pack(side=tk.LEFT, padx=5, pady=5)
+    root.mainloop()
